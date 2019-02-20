3764d80c8f88dd6f2938b20cb28c6d579f02a765
milespucarelli <miles.pucarelli@gmail.com>
2019-02-20 13:52:27 -0500

    Finished Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import Header from "./Components/Header"
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <header />;
+    return <Header header='WADDUUUPPPP'/>;
   }
 }
 
